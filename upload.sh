dd5d461b7801f9b8633967bd0dc48499946bf197
nitasn <46115487+nitasn@users.noreply.github.com>
2021-03-15 23:56:24 +0200

    update

--- upload.sh
+++ upload.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+# git init
+
 git add .
+
 git commit -m "update"
+
 git branch -M main
+
+# git remote add origin https://github.com/nitasn/nitasn.github.io.git
+
 git push -u origin main
+
 echo 'done'
